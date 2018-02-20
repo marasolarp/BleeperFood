a8daf895444e6468d59c06cc247616bc1d21bc53
marasolarp <mariasolarp@gmail.com>
2018-02-20 13:46:10 -0300

    muestra datos

--- assets/js/app.js
+++ assets/js/app.js
@@ -41,22 +41,23 @@ $('#btn-search').click(function() {
         // Acá pueden ir los append
         $('#card-insert').append('<div class="card">' +
           '<div class="card-image waves-effect waves-block waves-light">' +
-          '<img class="activator" src="' + {image}  + '">' +
+          '<img class="activator" src="' + image  + '">' +
           '</div>' +
           '<div class="card-content">' +
-          '<span class="card-title activator grey-text text-darken-4">' + {name} + '<i class="material-icons right">more_vert</i></span>' +
-          '<p>' + {cuisines} + '</p>' + '</div>' +
+          '<span class="card-title activator grey-text text-darken-4">' + name + '<i class="material-icons right">more_vert</i></span>' +
+          '<p>' + cuisines + '</p>' + '</div>' +
           '<div class="card-reveal">' +
-          '<span class="card-title grey-text text-darken-4">' + {name} + '<i class="material-icons right"> close</i></span>' +
+          '<span class="card-title grey-text text-darken-4">' + name + '<i class="material-icons right"> close</i></span>' +
           '<ul>' +
-          '<li>Dirección:' + {addres} + '</li>' +
-          '<li>Reserva:' + {reservation} + '</li>' +
-          '<li>Delivery:' + {deliveryOnline} + '</li>' +
-          '<li>Raiting:' + {rating} + '</li>' +
-          '<li>Votos:' + {votes} + '</li>' +
-          '<li>Comentarios:' + {ratingText} + '</li>' +
-          '</ul><div></div>')
+          '<li>Dirección:' + address + '</li>' +
+          '<li>Reserva:' + reservation + '</li>' +
+          '<li>Delivery:' + deliveryOnline + '</li>' +
+          '<li>Raiting:' + rating + '</li>' +
+          '<li>Votos:' + votes + '</li>' +
+          '<li>Comentarios:' + ratingText + '</li>' +
+          '</ul></div></div>')
       }
     }
   });
+  
 });
